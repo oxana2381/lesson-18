29e72618674ad3fe3e51e0ca76b7242e7801eba3
Oxana Ciobanu <oxanochka23@yahoo.com>
2020-05-13 17:41:10 +0300

    Accessing DOM2

--- index.js
+++ index.js
@@ -15,11 +15,6 @@ if(confirm('Do you like frontend?')){
     answer.textContent= 'NO';
 }
  
-
- 
-
- 
-
  
 
  //3//
@@ -35,18 +30,80 @@ ul.append (li);
 
 //4//
 
-let elem=document.querySelectorAll('li')[2];
-let elemlastChild;
+let lo=document.querySelectorAll('li ');
 
-elem.remove();
+let elem=document.getElementById('bread');
+ let k=elem
+for ( let k of lo ){
 
+ elem.remove(); };
 
+ 
 //5//
 let total=document.querySelector("p1");
 total=prompt('Your total price will be:')
 let price=document.getElementById("your-total-price");
 price.textContent=total;
 
+//6//
+ let product= 
+{ name:"Carrot" ,
+ price: 2.50 
+}
+
+    
+let quant=product.name;
+let number=product.price;
+
+
+if(prompt(' write item name')==product.name){
+ let quant=prompt(' quantity for this item','')
+
+let sum=quant*number ;
+ 
+price.textContent=sum;}
+
+else(alert( "Sorry the item you are searching is not found"));
+ 
+ 
+   
+
+
+
+
+
+
+
+
+
+//for (let item in product){
+ //alert(product.name )
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*let user= prompt('name','Carrot')
+ if (user="Carrot"){
+   prompt('write quantity ','')}
+else{(alert( "Sorry the item you are searching is notfound"))};*/
+
+
+//for (let index = 0; index < array.length; index++) {
+    //const element = array[index];
+    
 
 
 
